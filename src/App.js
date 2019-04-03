d10510f73d35618c09268d796b16e1fb769c965c
Haoyu ZUO <zli@palo-it.com>
2019-04-03 17:34:38 +0200

    Create Auth Object

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import { Route } from "react-router-dom";
+import React, {Component} from "react";
+import {Route} from "react-router-dom";
 import Home from "./Home";
 import Profile from "./Profile";
 import Nav from "./Nav";
@@ -8,10 +8,10 @@ class App extends Component {
   render() {
     return (
       <>
-        <Nav />
+        <Nav/>
         <div className="body">
-          <Route path="/" exact component={Home} />
-          <Route path="/profile" component={Profile} />
+          <Route path="/" exact component={Home}/>
+          <Route path="/profile" component={Profile}/>
         </div>
       </>
     );
--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import {BrowserRouter as Router, Route} from "react-router-dom";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <Router>
-    <Route component={App} />
+    <Route component={App}/>
   </Router>,
   document.getElementById("root")
 );
